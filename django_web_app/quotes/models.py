d19196ebf4a89ed4ac818243665d3a12a2a2f8a5
Jonathan Quah <quahjieren@gmail.com>
2019-07-31 18:22:07 +0800

    Updated Fav, Quote models; incorporated no of likes & fav

--- django_web_app/quotes/models.py
+++ django_web_app/quotes/models.py
@@ -8,16 +8,23 @@ class Quote(models.Model):
     title = models.CharField(max_length=100, default='title')
     content = models.TextField(max_length=1000)
     author = models.CharField(max_length=100, default='author')
-    date_posted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(auto_now_add=True)
     user_liked = models.ManyToManyField(User, related_name='user_liked', blank=True)  # check if additional options need to be added
     user_favourited = models.ManyToManyField(User, related_name='user_favourited', blank=True)
+    no_user_likes = models.PositiveIntegerField(default=0)
+    no_user_favourites = models.PositiveIntegerField(default=0)
+    check_liked = models.BooleanField(default=False)
+    check_favourited = models.BooleanField(default=False)
 
     def __str__(self):
         return self.content[:10]
 
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quote = models.ForeignKey(Quote, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
 
-class AuthorQuote(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
+class Favourite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quote = models.ForeignKey(Quote, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
--- django_web_app/quotes/views.py
+++ django_web_app/quotes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth.decorators import login_required
 from django.db.models.query import EmptyQuerySet
-from quotes.models import Quote
+from quotes.models import Quote, Like, Favourite
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.urls import reverse
 from django.contrib import messages
@@ -26,24 +26,24 @@ def home(request):
             for key, value in dict_item.items():
                 if key == 'content':
                     dict_item[key] = value.replace("<p>", "").replace("</p>", "").replace("<br />", "")
-                    # Replace numeric character references
-                    dict_item[key] = html.unescape(dict_item[key])
+                    dict_item[key] = html.unescape(dict_item[key])  # Replace numeric character references
                 
             quote = Quote()
             quote.author = dict_item['title']
             quote.content = dict_item['content']
 
-            # returns Queryset if quote content exists
-            check = Quote.objects.filter(content=quote.content).all()
+            check = Quote.objects.filter(content=quote.content).all()   # returns Queryset if quote content exists
 
             if not check:
-                # If not in database
-                print("Saving to database...")
+                print("Saving to database...") # If not in database
                 quote.save()
             else:
-                # If in database, store existing quote id into object sent for template rendering
-                quote.id = check.values("id")[0]['id']
-            
+                quote.id = check.values("id")[0]['id']  # If in database, store existing quote id into object sent for template rendering
+                
+                q = Quote.objects.get(content=quote.content)  # fetches quote from database
+                quote.no_user_likes = q.like_set.all().count()  # generate number of user likes for particular quote
+                quote.no_user_favourites = q.favourite_set.all().count()  # generate number of user likes for particular quote
+
             quotes.append(quote)
         
         # Paginate quotes
@@ -56,10 +56,6 @@ def home(request):
         except EmptyPage:
             quotes = quote_paginator.page(page.num_pages)
         
-        # Debugging
-        print("page=" + str(page))
-        print("posts=" + str(posts))
-        
         return render(request, 'quotes/home.html', {'quotes': quotes, 'posts': posts, 'page': page, 'max_posts':list(range(3, 6))})
             
     
@@ -70,20 +66,14 @@ def home(request):
         page = request.GET.get('page', 1)
 
         if request.user is not AnonymousUser and isinstance(request.POST.get('submit_user_like'), str):
-            # Fetch quote liked from existing database
-            quote_liked = get_object_or_404(Quote, id=request.POST.get('submit_user_like'))
-             
-            # Add user to user_liked field of quote
-            quote_liked.user_liked.add(request.user)
-            
+
+            # Creates Like entry in table quotes_like. Outputs (Like object, boolean on whether created)
+            Like.objects.get_or_create(user=request.user, quote_id = request.POST.get('submit_user_like'))
             messages.success(request, "Liked!")
+        
         elif request.user is not AnonymousUser and isinstance(request.POST.get('submit_user_favourite'), str):
-            # Fetch quote from existing database
-            quote_favourited = get_object_or_404(Quote, id=request.POST.get('submit_user_favourite'))
-             
-            # Add user to user_favourited field of quote
-            quote_favourited.user_favourited.add(request.user)
             
+            Favourite.objects.get_or_create(user=request.user, quote_id = request.POST.get('submit_user_favourite'))
             messages.success(request, "Favourited!")
         
         # Build redirect string back to 'quotes-home' with 'posts' and 'page' url query
