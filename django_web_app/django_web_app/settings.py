dce2b48e29118941c5a19ce44739d9ec561df7ff
Jonathan Quah <quahjieren@gmail.com>
2019-08-15 22:48:06 +0800

    Changed default.jpg, deleted socialauth

--- django_web_app/django_web_app/settings.py
+++ django_web_app/django_web_app/settings.py
@@ -25,7 +25,6 @@ mimetypes.add_type("image/svg+xml", ".svgz", True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -35,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] 
 
 
 """
@@ -47,7 +46,6 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'quotes.apps.QuotesConfig',
-    # 'socialauth.apps.SocialauthConfig'
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
--- django_web_app/socialauth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SocialauthConfig(AppConfig):
-    name = 'socialauth'
