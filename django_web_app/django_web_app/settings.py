e0acc8596aebe1656980eafa97372cf4986b3903
Jonathan Quah <quahjieren@gmail.com>
2019-08-24 08:07:56 +0800

    Updated settings.py file and requirements.txt

--- django_web_app/django_web_app/settings.py
+++ django_web_app/django_web_app/settings.py
@@ -137,7 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles_')
 STATIC_URL = '/static/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_URL = 'login'
